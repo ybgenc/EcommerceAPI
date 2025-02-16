2d50049aa990ed30a37f4029a0898be3db76dbc0
ybgenc <yusufbgenc@gmail.com>
2025-02-16 08:39:48 +0300

    Added signalR

--- Core/EcommerceAPI.Application/Features/Commands/Product/CreateProduct/CreateProductCommandHandler.cs
+++ Core/EcommerceAPI.Application/Features/Commands/Product/CreateProduct/CreateProductCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using EcommerceAPI.Application.Repositories.ProductRepository;
+﻿using EcommerceAPI.Application.Abstraction.Hubs;
+using EcommerceAPI.Application.Repositories.ProductRepository;
 using MediatR;
 
 namespace EcommerceAPI.Application.Features.Commands.Product.CreateProduct
@@ -6,16 +7,19 @@ namespace EcommerceAPI.Application.Features.Commands.Product.CreateProduct
     public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest, CreateProductCommandResponse>
     {
         readonly IProductWriteRepository _productWriteRepository;
+        readonly IProductHubService _productHubService;
 
-        public CreateProductCommandHandler(IProductWriteRepository productWriteRepository)
+        public CreateProductCommandHandler(IProductWriteRepository productWriteRepository, IProductHubService productHubService)
         {
             _productWriteRepository = productWriteRepository;
+            _productHubService = productHubService;
         }
 
         public async Task<CreateProductCommandResponse> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
         {
             await _productWriteRepository.AddAsync(new() { Name = request.Name, Price = request.Price, Stock = request.Stock });
             await _productWriteRepository.SaveAsync();
+            await _productHubService.ProductAddedMessageAsync($" new product {request.Name} added successfully");
             return new();
         }
     }
